bce2bd3ccdf91ada54fa2db3c3165b1e78a643ce
Jack Jeffress <jack@modular.co>
2021-07-27 16:33:11 +1000

    Added test files

---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,11 @@
+import withContentLayout from "layouts/withContentLayout";
+
+function PrivacyPolicy() {
+  return (
+    <div>
+      <h2>Privacy Policy</h2>
+    </div>
+  );
+}
+
+export default withContentLayout(PrivacyPolicy);
---
+++ pages/terms-and-conditions.tsx
@@ -0,0 +1,11 @@
+import withContentLayout from "layouts/withContentLayout";
+
+function TermsAndConditions() {
+  return (
+    <div>
+      <h2>Terms And Conditions</h2>
+    </div>
+  );
+}
+
+export default withContentLayout(TermsAndConditions);
