6f6376f0fce24b8cccb7076c4722dd06df3a45e1
Jack Jeffress <jack@modular.co>
2021-07-28 22:47:34 +1000

    Added temporary markers

--- lib/components/map/index.tsx
+++ lib/components/map/index.tsx
@@ -1,12 +1,21 @@
 import { memo } from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const center = {
-  lat: -33.79798,
-  lng: 151.28826,
+  lat: -33.79063314253369,
+  lng: 151.29607059265138,
 };
 
+const tempMarkers = [
+  [-33.76694771865754, 151.3131508996582],
+  [-33.79205976612762, 151.29958965087891],
+  [-33.80118959417948, 151.3201890161133],
+];
+
 function Map() {
+  const markers = tempMarkers.map(([lat, lng]) => {
+    return <Marker position={{ lat, lng }} key={`${lat}${lng}-marker`} />;
+  });
   return (
     <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
       <GoogleMap
@@ -21,7 +30,12 @@ function Map() {
           streetViewControl: false,
           disableDefaultUI: true,
         }}
-      ></GoogleMap>
+        onClick={({ latLng: { lat, lng } }) => {
+          console.log(lat(), lng());
+        }}
+      >
+        {markers}
+      </GoogleMap>
     </LoadScript>
   );
 }
