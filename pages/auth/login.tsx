0e00cb1769f1beb4d9e688459eb5fe4d9dac23ce
Jack Jeffress <jack@modular.co>
2021-08-03 11:35:29 +1000

    Added some files

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,12 +1,13 @@
 import { NextSeo } from "next-seo";
 import withContentLayout from "layouts/withContentLayout";
+import LoginForm from "components/auth/login";
 
 function Login() {
   return (
     <>
       <NextSeo title={`Migaloo - Login`} />
       <div>
-        <h2>Login</h2>
+        <LoginForm />
       </div>
     </>
   );
