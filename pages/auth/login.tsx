41da449022a9df5f8d1294a71a3473cdcbd22825
Jack Jeffress <jack@modular.co>
2021-08-19 21:18:13 +1000

    Tried to fix formik

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,13 +1,17 @@
 import { NextSeo } from "next-seo";
 import withContentLayout from "layouts/withContentLayout";
-import LoginForm from "components/auth/login";
+import dynamic from "next/dynamic";
+
+const MyLoginForm = dynamic(() => import("components/auth/login"), {
+  ssr: false,
+});
 
 function Login() {
   return (
     <>
       <NextSeo title={`Migaloo - Login`} />
       <div>
-        <LoginForm />
+        <MyLoginForm />
       </div>
     </>
   );
