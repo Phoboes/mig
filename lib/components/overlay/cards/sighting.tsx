20f49a311156e1d1b228a89313025c469f5de58b
Kane <Phoboes@live.com>
2021-08-28 15:44:12 +1000

    Fixing overlay error.

--- lib/components/overlay/cards/sighting.tsx
+++ lib/components/overlay/cards/sighting.tsx
@@ -4,7 +4,7 @@ export default function Sighting({ toggleState, sighting, speciesList }) {
   return (
     <Overlay toggleState={toggleState}>
       <h1 className="pt-4 text-blue-500 font-bold text-xl">
-        {sighting.species}
+        {sighting.species.common_name}
       </h1>
       <p className="text-blue-500 text-xs">{sighting.date}</p>
       <hr className="my-1 mt-2" />
